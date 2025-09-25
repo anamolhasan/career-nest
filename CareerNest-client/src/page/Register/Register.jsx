777619b773e5c9739a9ca4532f058f04ea2a85a2
anamolhasan <mdanamolhasan1998@gmail.com>
2025-09-25 18:23:28 +0600

    rigister form desing created

--- CareerNest-client/src/page/Register/Register.jsx
+++ CareerNest-client/src/page/Register/Register.jsx
@@ -1,23 +1,28 @@
+import Lottie from 'lottie-react'
 import React from 'react'
+
+import registerLottie from '../../assets/lottes/register.json'
 
 const Register = () => {
   return (
     <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
+  <div className="hero-content flex-col gap-5 lg:flex-row-reverse">
     <div className="text-center lg:text-left">
-     
+       <Lottie style={{width:'250px'}} animationData={registerLottie} loop={true}/>
     </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+    <div className="card  w-full max-w-sm shrink-0 ">
       <div className="card-body">
-         <h1 className="text-5xl font-bold">Login now!</h1>
-        <fieldset className="fieldset">
+         <h1 className="text-5xl font-bold">Register now!</h1>
+        <form >
+            <fieldset className="fieldset">
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name='name' className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name='password' className="input" placeholder="Password" />
           <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
+          <button className="btn btn-neutral mt-4">Register</button>
         </fieldset>
+        </form>
       </div>
     </div>
   </div>
